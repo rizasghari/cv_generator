8bee2244e1bf43ba67a29f40971b7410a265a0ff
rizasghari <developer.rizasghari@gmail.com>
2024-08-05 17:07:58 +0300

    cli template flag added

--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"flag"
 	"github.com/rizasgahri/cv_builder/configs"
 	"github.com/rizasgahri/cv_builder/internal/models"
 	"github.com/rizasgahri/cv_builder/internal/services"
 )
 
 func main() {
+	templateName := flag.String("template", "default", "Provide the template {default|urmu}")
+	flag.Parse()
+
+	template := models.Template{
+		Name: *templateName,
+	}
 
 	configs := configs.GetConfig()
 
 	generator := services.NewGeneratorService(configs)
 
-	template := models.Template{
-		Name: "urmu",
-	}
 	generator.Generate(template)
 }
